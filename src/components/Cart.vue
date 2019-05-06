a1242905fb06f6279ea7670a167741e1b2f49ecb
julienb86 <julien.bouafia@gmail.com>
2019-05-06 18:03:11 +0200

    completed

---
+++ src/components/Cart.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <ul class="list-group">
+        <li v-for="item in items" :key="item.id" class="list-group-item">
+            {{ item.title }} - ${{ item.price }}
+            <button @click="$emit('remove-from-cart', item)"  class="btn badge badge-danger float-right">Remove from Cart</button>
+        </li>
+    </ul>
+    <div class="card p-3 my-5">
+        <h5 class="text-center">
+            $ {{ total }}
+        </h5>
+    </div>
+    <button :disabled="items.length === 0" @click="$emit('pay')" class="btn btn-info form-control">Pay Now</button>
+</div>
+</template>
+
+<script>
+export default {
+    props : ["items"],
+
+    computed: {
+        total(){
+            return this.items.reduce((acc, item) => acc + Number(item.price), 0);
+        }
+    }
+}
+</script>
